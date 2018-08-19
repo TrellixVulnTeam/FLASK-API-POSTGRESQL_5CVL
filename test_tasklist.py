861e2cd38d9d275850d2c2dec29dbda174eaf0ca
imrankhannn81@gmail.com <imrankhannn81@gmail.com>
2018-08-20 00:20:04 +0500

    corrected links

--- test_tasklist.py
+++ test_tasklist.py
@@ -14,6 +14,7 @@ class TasklistTestCase(unittest.TestCase):
 
         # binds the app to the current context
         with self.app.app_context():
+            
             # create all tables
             db.create_all()
 
